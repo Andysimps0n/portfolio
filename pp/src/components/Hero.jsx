51757e5b8df9efb78995784c51fbc6c1ace31cf8
Andy Kim <26eckim@wonhago.org>
2025-05-22 22:19:15 +0900

    added lights

--- pp/src/components/Hero.jsx
+++ pp/src/components/Hero.jsx
@@ -1,30 +1,73 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { OrbitControls, Scroll, ScrollControls, useHelper   } from '@react-three/drei'
-import { DirectionalLightHelper } from 'three'
+import { DirectionalLightHelper, Group, RectAreaLight } from 'three'
 import HeroHTML from './HeroHTML';
 
-function Sphere() {
-    const sphereRef = useRef();
+
+import { RectAreaLightHelper } from 'three/addons/helpers/RectAreaLightHelper.js';
+import { RectAreaLightUniformsLib } from 'three/addons/lights/RectAreaLightUniformsLib.js';
+
+
+function RectLight() {
+    
+    const lightRef = useRef()
+    useHelper(lightRef, RectAreaLightHelper)
+    useEffect(()=>{
+        RectAreaLightUniformsLib.init();
+        // if(lightRef.current){
+        //     lightRef.current.lookAt(-10,0,0)
+        // }
+    },[])
+
+  return (
+    <rectAreaLight
+      ref={lightRef}
+      intensity={5}
+      color="orange"
+      width={4}
+      height={10}
+      position={[10, 0, 5]}
+      lookAt={[0, 0, 0]} // you can use a dummy object to face the light
+    />
+  )
+}
+
+
+function Torus() {
+    const torusRef = useRef();
     useFrame(()=>{
-        if (sphereRef.current) {
-            sphereRef.current.rotation.y += 0.005
-            // sphereRef.current.rotation.x += 0.005
+        if (torusRef.current) {
+            torusRef.current.rotation.y += 0.005
         }
     })
 
     return (
-        <mesh ref={sphereRef}>
-            <sphereGeometry args={[15, 32, 32]}></sphereGeometry>
-            <meshPhongMaterial  color="grey"></meshPhongMaterial>
+        <mesh position={[-10, 0, 0]} ref={torusRef}>
+            <torusKnotGeometry args={[3, 1, 100, 100]}></torusKnotGeometry>
+            <meshStandardMaterial metalness={0.9} roughness={0.01} color="white"></meshStandardMaterial>
         </mesh>
     )
 }
 
 function Light() {
-    const lightRef = useRef();
-    useHelper(lightRef, DirectionalLightHelper, 5, 'red')
-    return <directionalLight intensity={2} ref={lightRef} position={[0, 40, -1]} color="white"></directionalLight>
+
+    const redRef = useRef()
+    const blueRef = useRef()
+    const greenRef = useRef()
+
+    useHelper(blueRef, DirectionalLightHelper, 5, 'red')
+    useHelper(redRef, DirectionalLightHelper, 5, 'red')
+    useHelper(greenRef, DirectionalLightHelper, 5, 'red')
+    return (
+        <>
+        
+            <directionalLight intensity={10} ref={redRef} target-position={[-10, 0, 0]}  position={[-15, 0, 15]} color="#0xff0000"></directionalLight>
+            <directionalLight intensity={10} ref={blueRef} target-position={[-10, 0, 0]}  position={[-15, 0, -15]} color="#0x00ff00"></directionalLight>
+            <directionalLight intensity={10} ref={greenRef} target-position={[-10, 0, 0]}  position={[-15, 15, -10]} color="0x0000ff"></directionalLight>
+        </>
+    )
+
 
 }
 
@@ -32,26 +75,24 @@ function Light() {
 function Hero() {
   return (
     <>
-        <Canvas camera={{position : [30,30,30]}}>
+        <Canvas camera={{position : [0, 5, -20]}}>
 
 
-            <ScrollControls style={{ scrollbarWidth: 'none' }}>
+            <ScrollControls pages={3} damping={0} style={{ scrollbarWidth: 'none' }}>
                 <Scroll>
-                    <Sphere></Sphere>
+                    <Torus></Torus>
                 </Scroll>
                 <Scroll html>
                     <HeroHTML></HeroHTML>
                 </Scroll>
-
             </ScrollControls>
 
 
-
-
-
-            <ambientLight intensity={0.25} />
+            <RectLight/>
+            {/* <gridHelper args={[100, 50]} /> */}
+            <ambientLight intensity={0.7} />
             <Light></Light> 
-            <OrbitControls enableZoom={true}></OrbitControls>
+            <OrbitControls enableZoom={false}></OrbitControls>
         </Canvas>
     </>
   )
--- pp/src/components/HeroHTML.jsx
+++ pp/src/components/HeroHTML.jsx
@@ -5,10 +5,22 @@ function HeroHTML() {
     <div id="hero-scroll-container">
         <div className="hero-scroll-wrapper">
             <div className="header">
-                <div className="name">header-text</div>
+                <div className="header-text">Andy kim</div>
+                <div className="header-text">Contents</div>
             </div>
-            <div className="content"></div>
-            <div className="content"></div>
+            <div className="content">
+              <div>Contents</div>
+              <div>About</div>
+              <div>projects</div>
+              <div>Interests</div>
+              <div>Contents</div>
+            </div>
+            <div className="content" style={{textAlign : 'right'}}>
+              <div>Github</div>
+              <div>Instargram</div>
+              <div>010-1234-1234</div>
+              <div>LinkedIn</div>
+            </div>
         </div>
     </div>
   )
